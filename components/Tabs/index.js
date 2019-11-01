a0a99253aa6e7df4161c96f71146602919b73970
lukasjaronis <jaronisluke13@gmail.com>
2019-11-01 11:27:17 -0400

    tabs; axios is grabbing data succesfully. Done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -11,9 +11,9 @@
 
 
 entryPoint = document.querySelector('.topics');
-function TabCreator(e) { // e is just the items'elements we will pass in from the api through our function
+function TabCreator(e) { // e is just the items/elements we will pass in from the api through our function
 
-const create = e => document.create(e);
+const create = e => document.createElement(e);
 
 // setting up new elements
 
@@ -24,10 +24,24 @@ let newTab = create('div');
 newTab.classList.add('tab');
 
 // appending
+
 entryPoint.appendChild(newTab); // we are adding the newTab div to the HTML entry point 
 
-//
+// set content
+
+newTab.textContent = e; // the text for the newTab will be passed into the data (our data is .topics)
 
 
-
+return entryPoint;
 }
+
+axios
+.get("https://lambda-times-backend.herokuapp.com/topics")
+.then((dataResponse => { // we get a response from ^
+    console.log(dataResponse);
+    dataResponse.data.topics.forEach(item => TabCreator(item));
+    // 1. we see the response from the herouapp.
+    // 2. we look through the data
+    // 3. we look at the topics
+    // 4. we itterate through the topics and get the 'tabs', then pass each one into our function constructor. 
+}))
